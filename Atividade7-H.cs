5e6959dc0a88c5c67df219c4919d0e3ebc87d5b1
Caio <168545443+CaioUgo@users.noreply.github.com>
2025-02-05 19:25:40 -0300

    Atividade7-H.cs

---
+++ Atividade7-H.cs
@@ -0,0 +1,28 @@
+
+using System;
+
+namespace PATrilhasAtiv05
+{
+	class Program
+	{
+		public static void Main()
+		{	
+			int contador = 0;
+			
+			Console.WriteLine("Digite um número: ");
+			var numero = int.Parse(Console.ReadLine());
+			
+			Console.WriteLine("\n\rA tabuada do número {0} é: " ,numero);
+				
+			do {
+				int tabuada = numero * contador;
+				Console.WriteLine( +numero+ " · " +contador+ " = " +tabuada );
+				contador++;	
+			} while (contador < 11 );
+			
+			Console.WriteLine( );
+			Console.WriteLine("\n\rPress any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
