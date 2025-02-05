e04c31daf3527090cbf8fb7aead88365f6c6d681
Caio <168545443+CaioUgo@users.noreply.github.com>
2025-02-05 19:10:54 -0300

    Atividade7-E.cs

---
+++ Atividade7-E.cs
@@ -0,0 +1,39 @@
+
+using System;
+
+namespace PATrilhasAtiv05
+{
+	class Program
+	{
+		public static void Main()
+		{	
+			int soma = 0;
+			string sequencia;
+			
+			Console.WriteLine("Digite uma sequência de números inteiros(para encerrar a sequência digete algum núemro negativo): ");
+		
+			while (true)
+			{
+				sequencia = Console.ReadLine();
+				int numero;
+				
+				if (int.TryParse(sequencia, out numero) )
+				{
+					if (numero < 0) {
+						break;
+				    }
+					soma += numero;
+				}
+				
+				else {
+				    	Console.WriteLine("Valor inválido! Digite apenas números inteiros.");
+				    }
+				}
+			
+			Console.WriteLine("\n\rA soma dos números é {0}", soma);
+					  	
+			Console.Write("\n\rPress any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
